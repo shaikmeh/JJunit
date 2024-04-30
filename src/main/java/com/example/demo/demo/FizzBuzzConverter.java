81c1a9ab711b7b6ac237bb836a291f62d058728f
shaikmeh <shaikmeh@students.zhaw.ch>
2024-04-30 22:37:38 +0200

    first TDD

---
+++ src/main/java/com/example/demo/demo/FizzBuzzConverter.java
@@ -0,0 +1,12 @@
+package com.example.demo.demo;
+
+public class FizzBuzzConverter {
+    public String convert(int i) {
+        return "1";
+        }
+        
+    }
+
+            
+    
+
---
+++ src/test/java/com/example/demo/demo/FizzBuzzConverterTest.java
@@ -0,0 +1,54 @@
+package com.example.demo.demo;
+
+import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+//import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.assertj.core.api.Assert;
+
+
+
+
+public class FizzBuzzConverterTest {
+    @Test
+    public void fizzBuzzConverter1() {
+    FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+    assertEquals("1", fizzBuzz.convert(1));
+
+}
+}
+
+
+   // @Test
+  //   public void fizzBuzzConvertor2() {
+   //  FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+  //   assertEquals("2", fizzBuzz.convert(2));
+//}
+  //  @Test
+ //   public void fizzBuzzConvertor3() {
+  //  FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+ //   assertEquals("Fizz", fizzBuzz.convert(3));
+//}
+ //    @Test
+  //   public void fizzBuzzConvertorMultiplesOfThree() {
+ //    FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+ //    assertEquals("Fizz", fizzBuzz.convert(6));
+//}
+ //    @Test
+//     public void fizzBuzzConvertorMultiplesOfSeven() {
+//     FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+  //   assertEquals("Buzz", fizzBuzz.convert(7));
+//}
+  //  @Test
+ //   public void fizzBuzzConvertorMultiplesOfThreeAndSeven() {
+   // FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+  //  assertNotEquals("FizzBuzz", fizzBuzz.convert(14));
+   // assertEquals("FizzBuzz", fizzBuzz.convert(21));
+  //  assertEquals("FizzBuzz", fizzBuzz.convert(42));
+  //  assertEquals("FizzBuzz", fizzBuzz.convert(63));
+//}
+//}
+    
+
