3fb473a6a7b37a5cba1c737398e51bd762a3c9cc
shaikmeh <shaikmeh@students.zhaw.ch>
2024-05-01 17:08:01 +0200

    4TDD code korrigiert

--- demo/src/main/java/com/example/demo/demo/FizzBuzzConverter.java
+++ demo/src/main/java/com/example/demo/demo/FizzBuzzConverter.java
@@ -2,10 +2,12 @@ package com.example.demo.demo;
 
 public class FizzBuzzConverter {
     public String convert(int i) {
-        if (i%3 == 0) {
+        if (i % 3 == 0) {
             return "Fizz";
+            } else if (i % 7 == 0) {
+            return "Buzz";
             }
             return String.valueOf(i);
-        
+            
 }
 }
