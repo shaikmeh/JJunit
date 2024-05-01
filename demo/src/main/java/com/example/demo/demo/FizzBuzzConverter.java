9bcf470a6051e97eb6204b1dcfd8b36a65496ad8
shaikmeh <shaikmeh@students.zhaw.ch>
2024-05-01 17:16:16 +0200

    Code Korrigiert all pass

--- demo/src/main/java/com/example/demo/demo/FizzBuzzConverter.java
+++ demo/src/main/java/com/example/demo/demo/FizzBuzzConverter.java
@@ -2,7 +2,9 @@ package com.example.demo.demo;
 
 public class FizzBuzzConverter {
     public String convert(int i) {
-        if (i % 3 == 0) {
+        if (i % 3 == 0 && i % 7 == 0) {
+            return "FizzBuzz";
+            } else if (i % 3 == 0) {
             return "Fizz";
             } else if (i % 7 == 0) {
             return "Buzz";
