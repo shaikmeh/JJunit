503ebdaadde6cd7448e866d83520e966dfb963f6
shaikmeh <shaikmeh@students.zhaw.ch>
2024-05-01 15:16:03 +0200

    1st commit

---
+++ demo/src/main/java/com/example/demo/demo/FizzBuzzConverter.java
@@ -0,0 +1,8 @@
+package com.example.demo.demo;
+
+public class FizzBuzzConverter {
+    public String convert(int i) {
+        return "1";
+        }
+        
+}
---
+++ demo/src/test/java/com/example/demo/demo/FizzBuzzConverterTest.java
@@ -0,0 +1,23 @@
+package com.example.demo.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzConverterTest {
+    @Test
+    public void fizzBuzzConverter1() {
+    FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+    assertEquals("1", fizzBuzz.convert(1));
+}
+
+    
+    }
+
+
+    
+
+
+
+    
+
